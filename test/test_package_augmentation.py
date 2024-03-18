colcon/colcon-installed-package-information

commit 5a3219496da5b8c4568b54857d09616c4b386c49

Switch from pkg_resources to importlib.metadata (#7)
This is a fairly clean swap, but one unintended side effect I noticed is
that the "extras" dependencies are now enumerated as runtime
dependencies. This is probably the right behavior for a typical "extra",
but isn't really right for the "test" extras we use to declare test
dependencies because these packages have already been installed and
therefore will never need their test dependencies again.

Some refactoring in colcon-core may be able to alleviate this.

colcon_installed_package_information/package_augmentation/python.py
@@ -3,13 +3,17 @@
 
 import sysconfig
 
+try:
+    from importlib.metadata import Distribution
+except ImportError:
+    # TODO: Drop this when dropping Python 3.7 support
+    from importlib_metadata import Distribution
+
 from colcon_core.package_augmentation \
     import PackageAugmentationExtensionPoint
 from colcon_core.package_augmentation.python \
     import create_dependency_descriptor
 from colcon_core.plugin_system import satisfies_version
-from pkg_resources import Environment
-from pkg_resources import Requirement
 
 
 class InstalledPythonPackageAugmentation(PackageAugmentationExtensionPoint):
@@ -32,24 +36,24 @@ class InstalledPythonPackageAugmentation(PackageAugmentationExtensionPoint):
         if not descs:
             return
 
-        environments = {}
         for desc in descs:
-            key = Requirement.parse(desc.name).key
-            for lib_dir in _enumerate_python_dirs(str(desc.path)):
-                if lib_dir not in environments:
-                    environments[lib_dir] = Environment([lib_dir])
-                dist = next(iter(environments[lib_dir][key]), None)
-                if dist:
-                    break
-            else:
+            dists = Distribution.discover(
+                path=_enumerate_python_dirs(str(desc.path)),
+                name=desc.name)
+            try:
+                dist = next(iter(dists))
+            except IndexError:
                 continue
 
-            if dist.version and not desc.metadata.get('version'):
-                desc.metadata['version'] = dist.version
+            version = dist.version
+            if version and not desc.metadata.get('version'):
+                desc.metadata['version'] = version
             desc.type = 'installed.python'
+            # TODO: We should find a clean way to exclude test-oriented extras
+            #       from this enumeration.
             desc.dependencies['run'].update(
                 create_dependency_descriptor(str(req))
-                for req in dist.requires())
+                for req in dist.requires)
 
 
 def _enumerate_python_dirs(prefix):

test/spell_check.words
@@ -2,6 +2,8 @@ apache
 colcon
 deps
 descs
+dists
+importlib
 iterdir
 linter
 noqa
@@ -18,3 +20,4 @@ scspell
 setuptools
 sysconfig
 thomas
+todo

setup.cfg
@@ -27,6 +27,7 @@ keywords = colcon
 python_requires = >=3.6
 install_requires =
   colcon-core
+  importlib-metadata; python_version < "3.8"
 packages = find:
 zip_safe = true
 

stdeb.cfg
@@ -1,5 +1,5 @@
 [colcon-installed-package-information]
 No-Python2:
-Depends3: python3-colcon-core
+Depends3: python3-colcon-core, python3 (>= 3.8) | python3-importlib-metadata
 Suite: focal jammy noble bookworm trixie
 X-Python3-Version: >= 3.6

commit 92600b91c818a32b076263bb694610144a4d669e

Fix some regressions in package_augmentation.python
There were some bugs introduced in the importlib.metadata change which I
somehow didn't hit during my manual testing. This time, I added tests so
that it doesn't happen again.

Fixes: 5a3219496da5b8c4568b54857d09616c4b386c49

colcon_installed_package_information/package_augmentation/python.py
@@ -42,18 +42,21 @@ class InstalledPythonPackageAugmentation(PackageAugmentationExtensionPoint):
                 name=desc.name)
             try:
                 dist = next(iter(dists))
-            except IndexError:
+            except StopIteration:
                 continue
 
-            version = dist.version
-            if version and not desc.metadata.get('version'):
-                desc.metadata['version'] = version
+            if not desc.metadata.get('version'):
+                version = dist.version
+                if version:
+                    desc.metadata['version'] = version
             desc.type = 'installed.python'
             # TODO: We should find a clean way to exclude test-oriented extras
             #       from this enumeration.
-            desc.dependencies['run'].update(
-                create_dependency_descriptor(str(req))
-                for req in dist.requires)
+            requires = dist.requires
+            if requires is not None:
+                desc.dependencies['run'].update(
+                    create_dependency_descriptor(str(req))
+                    for req in requires)
 
 
 def _enumerate_python_dirs(prefix):

test/spell_check.words
@@ -1,4 +1,5 @@
 apache
+cmake
 colcon
 deps
 descs

test/test_package_augmentation.py
@@ -1,0 +1,136 @@
+# Copyright 2024 Open Source Robotics Foundation, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from pathlib import Path
+import sys
+import sysconfig
+
+from colcon_core.location import get_relative_package_index_path
+from colcon_core.package_descriptor import PackageDescriptor
+from colcon_installed_package_information.package_augmentation.colcon_index \
+    import ColconIndexPackageAugmentation
+from colcon_installed_package_information.package_augmentation.python \
+    import InstalledPythonPackageAugmentation
+import pytest
+
+
+@pytest.fixture
+def mock_egg_package(tmp_path):
+    lib_dir = Path(sysconfig.get_path('purelib', vars={'base': str(tmp_path)}))
+    pkg_dir = lib_dir / 'egg_pkg-4.5.6.egg-info'
+
+    pkg_dir.mkdir(parents=True)
+    (pkg_dir / 'PKG-INFO').write_text(
+        'Metadata-Version: 2.1\n'
+        'Name: egg-pkg\n'
+        'Version: 4.5.6\n'
+    )
+    (pkg_dir / 'requires.txt').write_text(
+        'wheel-pkg\n'
+    )
+
+    desc = PackageDescriptor(str(tmp_path))
+    desc.name = 'egg-pkg'
+    desc.type = 'installed'
+    yield desc
+
+
+@pytest.fixture
+def mock_wheel_package(tmp_path):
+    lib_dir = Path(sysconfig.get_path('purelib', vars={'base': str(tmp_path)}))
+    pkg_dir = lib_dir / (
+        'wheel_pkg-1.2.3.'
+        f'py{sys.version_info.major}.{sys.version_info.minor}.dist-info'
+    )
+
+    pkg_dir.mkdir(parents=True)
+    (pkg_dir / 'METADATA').write_text(
+        'Metadata-Version: 2.1\n'
+        'Name: wheel-pkg\n'
+        'Version: 1.2.3\n'
+    )
+
+    desc = PackageDescriptor(str(tmp_path))
+    desc.name = 'wheel-pkg'
+    desc.type = 'installed'
+    yield desc
+
+
+@pytest.fixture
+def mock_colcon_index_package(tmp_path):
+    index_dir = tmp_path / get_relative_package_index_path()
+
+    index_dir.mkdir(parents=True)
+    (index_dir / 'colcon-index-pkg').write_text(
+        'some-other-pkg',
+    )
+
+    desc = PackageDescriptor(str(tmp_path))
+    desc.name = 'colcon-index-pkg'
+    desc.type = 'installed'
+    yield desc
+
+
+def test_colcon_index(mock_colcon_index_package):
+    augmentation_extension = ColconIndexPackageAugmentation()
+
+    augmentation_extension.augment_packages([
+        mock_colcon_index_package,
+    ])
+
+    assert mock_colcon_index_package.name == 'colcon-index-pkg'
+    assert mock_colcon_index_package.type == 'installed.colcon'
+    assert 'some-other-pkg' in mock_colcon_index_package.dependencies['run']
+
+
+@pytest.mark.parametrize('augmentation_extension_class', [
+    ColconIndexPackageAugmentation,
+    InstalledPythonPackageAugmentation,
+])
+def test_no_installed(tmp_path, augmentation_extension_class):
+    augmentation_extension = augmentation_extension_class()
+
+    desc = PackageDescriptor(str(tmp_path))
+    desc.name = 'cmake-package'
+    desc.type = 'cmake'
+
+    augmentation_extension.augment_packages([desc])
+
+    assert desc.name == 'cmake-package'
+    assert desc.type == 'cmake'
+
+
+@pytest.mark.parametrize('augmentation_extension_class', [
+    ColconIndexPackageAugmentation,
+    InstalledPythonPackageAugmentation,
+])
+def test_not_found(tmp_path, augmentation_extension_class):
+    augmentation_extension = augmentation_extension_class()
+
+    desc = PackageDescriptor(str(tmp_path))
+    desc.name = 'no-such-package'
+    desc.type = 'installed'
+
+    augmentation_extension.augment_packages([desc])
+
+    assert desc.name == 'no-such-package'
+    assert desc.type == 'installed'
+
+
+def test_python(mock_egg_package, mock_wheel_package):
+    augmentation_extension = InstalledPythonPackageAugmentation()
+
+    augmentation_extension.augment_packages([
+        mock_egg_package,
+        mock_wheel_package,
+    ])
+
+    assert mock_egg_package.name == 'egg-pkg'
+    assert mock_egg_package.type == 'installed.python'
+    assert mock_egg_package.metadata.get('version') == '4.5.6'
+    assert 'wheel-pkg' in mock_egg_package.dependencies['run']
+
+    assert mock_wheel_package.name == 'wheel-pkg'
+    assert mock_wheel_package.type == 'installed.python'
+    assert mock_wheel_package.metadata.get('version') == '1.2.3'
+    assert not any(mock_wheel_package.dependencies.items())